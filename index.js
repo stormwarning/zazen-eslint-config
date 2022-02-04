25920862d57f8956a886dfc35072a6579fcb92c0
Jeff <rustyangel@gmail.com>
2022-02-03 21:38:26 -0700

    Add custom rule settings from XO

--- index.js
+++ index.js
@@ -33,6 +33,42 @@ module.exports = {
          */
         'prefer-const': 'off',
         'prefer-let/prefer-let': 'error',
+
+        'unicorn/consistent-destructuring': 'off',
+        'unicorn/consistent-function-scoping': 'off',
+        /** https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/expiring-todo-comments.md */
+        'unicorn/expiring-todo-comments': ['warn', { terms: ['@todo'] }],
+        'unicorn/no-null': 'off',
+        'unicorn/no-useless-undefined': 'off',
+        'unicorn/prefer-ternary': ['error', 'only-single-line'],
+
+        'promise/param-names': 'error',
+        'promise/no-return-wrap': ['error', { allowReject: true }],
+        'promise/no-new-statics': 'error',
+        'promise/no-return-in-finally': 'error',
+        'promise/valid-params': 'error',
+        'promise/prefer-await-to-then': 'error',
+
+        'import/default': 'error',
+        'import/export': 'error',
+        'import/extensions': ['error', 'always', { ignorePackages: true }],
+        'import/first': 'error',
+        'import/named': 'error',
+        'import/namespace': ['error', { allowComputed: true }],
+        'import/newline-after-import': 'error',
+        'import/no-absolute-path': 'error',
+        'import/no-amd': 'error',
+        'import/no-anonymous-default-export': 'error',
+        'import/no-cycle': ['error', { ignoreExternal: true }],
+        'import/no-duplicates': 'error',
+        'import/no-extraneous-dependencies': 'error',
+        'import/no-mutable-exports': 'error',
+        'import/no-named-as-default-member': 'error',
+        'import/no-named-as-default': 'error',
+        'import/no-named-default': 'error',
+        'import/no-self-import': 'error',
+        'import/no-useless-path-segments': 'error',
+        'import/no-webpack-loader-syntax': 'error',
     },
     overrides: [
         {
@@ -48,6 +84,8 @@ module.exports = {
                 'plugin:prettier/recommended',
             ],
             rules: {
+                'import/named': 'off',
+
                 'etc/prefer-less-than': 'error',
             },
         },
--- node.js
+++ node.js
@@ -2,6 +2,38 @@
 
 module.exports = {
     plugins: ['n'],
-    extends: ['plugin:n/recommended'],
-    rules: {},
+    /**
+     * @see https://github.com/weiran-zsd/eslint-plugin-node
+     */
+    extends: ['plugin:n/recommended', 'plugin:prettier/recommended'],
+    rules: {
+        /**
+         * We have this enabled in addition to `import/extensions` as
+         * this one has an auto-fix.
+         */
+        'n/file-extension-in-import': [
+            'error',
+            'always',
+            /**
+             * TypeScript doesn't yet support using extensions and
+             * fails with error TS2691.
+             */
+            { '.ts': 'never', '.tsx': 'never' },
+        ],
+        'n/no-deprecated-api': 'error',
+        'n/no-mixed-requires': ['error', { grouping: true, allowCall: true }],
+        'n/no-new-require': 'error',
+        'n/no-path-concat': 'error',
+        'n/no-unpublished-bin': 'error',
+        'n/prefer-global/buffer': ['error', 'never'],
+        'n/prefer-global/console': ['error', 'always'],
+        'n/prefer-global/process': ['error', 'never'],
+        'n/prefer-global/text-decoder': ['error', 'always'],
+        'n/prefer-global/text-encoder': ['error', 'always'],
+        'n/prefer-global/url-search-params': ['error', 'always'],
+        'n/prefer-global/url': ['error', 'always'],
+        'n/prefer-promises/dns': 'error',
+        'n/prefer-promises/fs': 'error',
+        'n/process-exit-as-throw': 'error',
+    },
 }
