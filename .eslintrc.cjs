3281e8ca85e3598e20fa30924a0d93c0eeccaba8
Jeff <rustyangel@gmail.com>
2022-02-03 22:49:58 -0700

    Disable rules per XO

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,14 @@
 module.exports = {
     plugins: ['unicorn'],
     extends: ['./index.js', './node.js'],
+    parserOptions: {
+        sourceType: 'module',
+    },
     env: {
         node: true,
     },
     rules: {
         'unicorn/prefer-module': 'off',
+        'n/no-unsupported-features/es-syntax': 'off',
     },
 }
--- index.js
+++ index.js
@@ -67,6 +67,7 @@ module.exports = {
         'import/no-named-as-default': 'error',
         'import/no-named-default': 'error',
         'import/no-self-import': 'error',
+        'import/no-unresolved': 'off',
         'import/no-useless-path-segments': 'error',
         'import/no-webpack-loader-syntax': 'error',
     },
